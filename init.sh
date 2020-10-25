cdf280e041700fe4df255e766ade7ec834a606f6
MiX1K <emirates.95.mhard@gmail.com>
2020-10-25 11:48:06 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo gunicorn -c /home/box/web/etc/gun.conf ask.wsgi:application
